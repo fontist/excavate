8f0f9b03ab14c8cc38f8565c1193b5e211e5751e
Alexey Morozov <alexey.morozov.is@gmail.com>
2021-04-01 11:13:46 +0300

    Remove the xar extractor until it requires less dependencies (lz4 issue)

--- spec/excavate/archive_spec.rb
+++ spec/excavate/archive_spec.rb
@@ -80,12 +80,6 @@ RSpec.describe Excavate::Archive do
       include_examples "yields filename", "Marlett.ttf"
     end
 
-    context "xar" do
-      let(:archive_example) { "archive.pkg" }
-
-      include_examples "yields filename", "Payload"
-    end
-
     context "zip" do
       let(:archive_example) { "fonts.zip" }
 
@@ -121,12 +115,6 @@ RSpec.describe Excavate::Archive do
         let(:archive_example) { "fonts.src.rpm" }
 
         include_examples "yields filename recursively", "Example.txt"
-      end
-
-      context "pkg" do
-        let(:archive_example) { "archive.pkg" }
-
-        include_examples "yields filename recursively", "file.txt"
       end
 
       context "failing subarchive" do
