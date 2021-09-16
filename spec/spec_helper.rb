b50361874a303c903074663744a1eda63905223e
Alexey Morozov <alexey.morozov.is@gmail.com>
2021-09-16 21:36:33 +0300

    Extract particular file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require_relative "../lib/excavate"
 
+Dir["./spec/support/**/*.rb"].sort.each { |file| require file }
+
 RSpec.configure do |config| # rubocop:disable Style/SymbolProc
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
