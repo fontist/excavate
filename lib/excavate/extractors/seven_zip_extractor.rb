bd0bc46305fb90b7c49ccf8977d2404666d61b68
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-10-27 13:00:12 +0300

    Added test for 7z self-extracting archive with BCJ and LZMA1 filters

--- lib/excavate/extractors/seven_zip_extractor.rb
+++ lib/excavate/extractors/seven_zip_extractor.rb
@@ -1,23 +1,14 @@
-require "ffi-libarchive-binary"
+require "seven_zip_ruby"
 
 module Excavate
   module Extractors
     class SevenZipExtractor < Extractor
       def extract(target)
         Dir.chdir(target) do
-          extract_with_libarchive
+          File.open(@archive, "rb") do |file|
+            SevenZipRuby::Reader.extract_all(file, target)
+          end
         end
-      end
-
-      def extract_with_libarchive
-        flags = ::Archive::EXTRACT_PERM
-        reader = ::Archive::Reader.open_filename(@archive)
-
-        reader.each_entry do |entry|
-          reader.extract(entry, flags.to_i)
-        end
-
-        reader.close
       end
     end
   end
