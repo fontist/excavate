2de930613a71bd4aec0af4b2e6de535c8f3e6e38
Alexey Morozov <alexey.morozov.is@gmail.com>
2023-08-22 19:54:28 +0200

    Detect fake gzip files

--- lib/excavate/file_magic.rb
+++ lib/excavate/file_magic.rb
@@ -9,9 +9,15 @@ module Excavate
     end
 
     def detect
-      case File.read(@path, 8, mode: "rb")
+      beginning = File.read(@path, 8, mode: "rb")
+      case beginning
       when "MSCF\x00\x00\x00\x00".force_encoding("BINARY")
         :cab
+      else
+        case beginning.byteslice(0, 2)
+        when "\x1F\x8B".force_encoding("BINARY")
+          :gzip
+        end
       end
     end
   end
